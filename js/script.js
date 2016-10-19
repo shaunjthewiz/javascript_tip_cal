5b4aa4d8d099b3ef204c8c0510d668099e222cae
shaunjthewiz <shaunjgov1@gmail.com>
2016-10-19 17:36:23 -0400

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-var bill = parseInt(prompt("Input a number."), 10)
+var bill = parseInt(prompt("How Much is your total bill."), 10)
 var tip = .20
 var total = bill * tip + bill
 alert(total + " is your total bill plus tip");
